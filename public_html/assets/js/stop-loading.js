5eb4607e1b5d641ad28c144addd11a333145b44e
dev-henen <henen.dev@gmail.com>
2024-12-22 14:39:53 +0100

    Completed the home page

--- public_html/assets/js/stop-loading.js
+++
@@ -1,9 +0,0 @@
-function stopLoading() {
-    document.getElementById("appLoader").style.display = "none";
-}
-var load = setInterval(() => {
-    if(document.readyState === 'complete') {
-        stopLoading();
-        clearInterval(load);
-    }
-}, 500);
