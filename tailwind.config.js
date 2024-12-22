93666f7ff64cb2d1e5ef52b9c772295027b45153
dev-henen <henen.dev@gmail.com>
2024-12-22 02:01:53 +0100

    Enhanced setuo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,41 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     './app/templates/**/*.twig',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bitter: ["Arial", "Helvetica"],
+        worksans: ["Arial", "sans-serif"],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#000',
+            a: {
+              color: '##0E55B6',
+              '&:hover': {
+                color: '#D8302F',
+              },
+            },
+          },
+        },
+      }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'gray': colors.gray,
+      'black': colors.black,
+      'white': colors.white,
+      'light': '#EEFEF8',
+      'lighter': '#EEFEF8',
+      'dark': '#121212',
+      'primary': '#0E55B6',
+    },
   },
   plugins: [],
 };
